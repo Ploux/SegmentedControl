5501e22941b1420cd858ef010df011b2280a967a
Ploux <peter@humo.golf>
2024-05-28 18:29:07 -0500

    Initial Commit

---
+++ SegmentedControl/SegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlApp.swift
+//  SegmentedControl
+//
+//  Created by Peter Loux on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
